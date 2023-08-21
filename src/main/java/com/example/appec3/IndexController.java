f6296aea80fdbf35870902c3461376fc18727cad
kimkiju01 <kimkiju01@gmail.com>
2023-08-21 11:36:58 +0900

    ver2

--- src/main/java/com/example/appec3/IndexController.java
+++ src/main/java/com/example/appec3/IndexController.java
@@ -1,2 +1,25 @@
-package com.example.appec3;public class IndexController {
+package com.example.appec3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/goodbye")
+public class IndexController {
+
+    @GetMapping("/bye")
+    public String bye(Model model){
+        model.addAttribute("aa","hye");
+        return "bye";
+    }
+
+    @GetMapping("/night")
+    @ResponseBody
+    public String night(){
+       return "aaa";
+    }
+
 }
--- src/main/java/com/example/appec3/SampleController.java
+++ src/main/java/com/example/appec3/SampleController.java
@@ -1,2 +1,16 @@
-package com.example.appec3;public class SampleController {
+package com.example.appec3;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hi")
+public class SampleController {
+
+    @GetMapping("/kiju")
+    public String kiju(){
+
+        return "hi";
+    }
 }
